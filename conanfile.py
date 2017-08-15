c2df24861efe310732f6ee727fbd243aa2b8ef83
Jerry W <jerrywiltse@gmail.com>
2017-08-14 21:39:23 -0400

    mass update - minor recipe and cosmetic readme

--- conanfile.py
+++ conanfile.py
@@ -3,15 +3,15 @@ from conans import ConanFile, tools, os
 class BoostType_TraitsConan(ConanFile):
     name = "Boost.Type_Traits"
     version = "1.64.0"
-    generators = "txt"
-    url = "https://github.com/boostorg/type_traits"
+    url = "https://github.com/bincrafters/conan-boost-type_traits"
+    source_url = "https://github.com/boostorg/type_traits"
     description = "For a description of this library, please visit http://boost.org/type_traits "
     license = "www.boost.org/users/license.html"
     lib_short_name = "type_traits"
 
     def source(self):
         self.run("git clone --depth=50 --branch=boost-{0} {1}.git"
-                 .format(self.version, self.url))
+                 .format(self.version, self.source_url))
 
     def package(self):
         include_dir = src=os.path.join(os.getcwd(), self.lib_short_name, "include")
