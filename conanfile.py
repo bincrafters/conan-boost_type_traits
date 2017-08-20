bf2ffedbc9f3bf16a4c0e88abb2c50e618c1aab3
Jerry W <jerrywiltse@gmail.com>
2017-08-20 00:43:55 -0400

    enabled short paths

--- conanfile.py
+++ conanfile.py
@@ -3,6 +3,7 @@ from conans import ConanFile, tools, os
 class BoostType_TraitsConan(ConanFile):
     name = "Boost.Type_Traits"
     version = "1.64.0"
+    short_paths = True
     url = "https://github.com/bincrafters/conan-boost-type_traits"
     source_url = "https://github.com/boostorg/type_traits"
     description = "For a description of this library, please visit http://boost.org/type_traits "
@@ -14,7 +15,7 @@ class BoostType_TraitsConan(ConanFile):
 
     def source(self):
         for lib_short_name in self.lib_short_names:
-            self.run("git clone --depth=50 --branch=boost-{0} https://github.com/boostorg/{1}.git"
+            self.run("git clone --depth=1 --branch=boost-{0} https://github.com/boostorg/{1}.git"
                      .format(self.version, lib_short_name)) 
 
     def package(self):
