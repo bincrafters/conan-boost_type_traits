9a720c025d3c92ac6fc7c1ce206c1f721cabbf8a
Jerry Wiltse <jerrywiltse@gmail.com>
2017-09-17 11:35:07 -0400

    Updated version to 1.65.1

--- conanfile.py
+++ conanfile.py
@@ -2,15 +2,15 @@ from conans import ConanFile, tools, os
 
 class BoostType_TraitsConan(ConanFile):
     name = "Boost.Type_Traits"
-    version = "1.64.0"
+    version = "1.65.1"
     short_paths = True
     url = "https://github.com/bincrafters/conan-boost-type_traits"
     description = "For a description of this library, please visit http://boost.org/type_traits "
     license = "www.boost.org/users/license.html"
     lib_short_names = ["type_traits"]
-    requires = "Boost.Config/1.64.0@bincrafters/testing", \
-                      "Boost.Core/1.64.0@bincrafters/testing", \
-                      "Boost.Static_Assert/1.64.0@bincrafters/testing"
+    requires = "Boost.Config/1.65.1@bincrafters/testing", \
+                      "Boost.Core/1.65.1@bincrafters/testing", \
+                      "Boost.Static_Assert/1.65.1@bincrafters/testing"
 
     def source(self):
         boostorg_github = "https://github.com/boostorg"
