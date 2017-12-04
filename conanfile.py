7140976026c8f9c9f2c8d4085d5c7ee1bcf088f2
Rene Rivera <grafikrobot@gmail.com>
2017-12-03 23:37:31 -0600

    Next generation of Boost packages.

--- conanfile.py
+++ conanfile.py
@@ -1,30 +1,38 @@
 from conans import ConanFile, tools
-import os
+
 
 class BoostType_TraitsConan(ConanFile):
     name = "Boost.Type_Traits"
     version = "1.65.1"
-    short_paths = True
     url = "https://github.com/bincrafters/conan-boost-type_traits"
     description = "For a description of this library, please visit http://boost.org/type_traits "
     license = "www.boost.org/users/license.html"
+
+    requires = \
+        "Boost.Config/1.65.1@bincrafters/testing", \
+        "Boost.Core/1.65.1@bincrafters/testing", \
+        "Boost.Static_Assert/1.65.1@bincrafters/testing"
+
     lib_short_names = ["type_traits"]
-    requires = "Boost.Config/1.65.1@bincrafters/testing", \
-                      "Boost.Core/1.65.1@bincrafters/testing", \
-                      "Boost.Static_Assert/1.65.1@bincrafters/testing"
+    is_header_only = True
 
-    def source(self):
-        boostorg_github = "https://github.com/boostorg"
-        archive_name = "boost-" + self.version  
-        for lib_short_name in self.lib_short_names:
-            tools.get("{0}/{1}/archive/{2}.tar.gz"
-                .format(boostorg_github, lib_short_name, archive_name))
-            os.rename(lib_short_name + "-" + archive_name, lib_short_name)
+    # BEGIN
 
-    def package(self):
-        for lib_short_name in self.lib_short_names:
-            include_dir = os.path.join(lib_short_name, "include")
-            self.copy(pattern="*", dst="include", src=include_dir)
+    short_paths = True
+    build_requires = "Boost.Generator/1.65.1@bincrafters/testing"
 
     def package_id(self):
-        self.info.header_only()
+        if self.is_header_only:
+            self.info.header_only()
+
+    @property
+    def env(self):
+        try:
+            with tools.pythonpath(super(self.__class__, self)):
+                import boostgenerator # pylint: disable=F0401
+                boostgenerator.BoostConanFile(self)
+        except:
+            pass
+        return super(self.__class__, self).env
+
+    # END
